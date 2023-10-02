487f2c94d559b3ba65730102b4dde21bfbe4ae4d
Alex-R-Projects <mralexreyes99@gmail.com>
2023-10-02 15:44:58 -0700

    Started restocker instructions

---
+++ restocker_instructs.py
@@ -0,0 +1,9 @@
+# This will be the file that takes in the pushed out instructions FROM the management team/tool
+
+# [Here we can place]
+
+
+
+class Restocker: # In order to get the instructions, the restocker, I think should have some sort of account to get 
+    def __init__(self) :
+    pass

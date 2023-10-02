af47e1cb71c94890bdbaf6ef77ccad7003e5a009
Alex-R-Projects <mralexreyes99@gmail.com>
2023-10-02 16:37:23 -0700

    added some more comments

--- restocker_instructs.py
+++ restocker_instructs.py
@@ -1,9 +1,9 @@
 # This will be the file that takes in the pushed out instructions FROM the management team/tool
 
-# [Here we can place]
+# [Here we can place the other files like main, etc]
 
-
+# Idea I was thinking of each restocker to have a phone number to get access to the instructions
 
 class Restocker: # In order to get the instructions, the restocker, I think should have some sort of account to get 
-    def __init__(self) :
-    pass
+    def __init__(self) : # constructor
+        pass

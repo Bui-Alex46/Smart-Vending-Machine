4c603c4398b5241ce8ea4ab8b6838ed16bc4ad98
Alex-R-Projects <mralexreyes99@gmail.com>
2023-10-02 17:31:41 -0700

    added code to fix window close error

--- restock.py
+++ restock.py
@@ -2,10 +2,14 @@ import PySimpleGUI as sg
 
 def create_restock_window():
     sg.theme('LightGreen')
+    event = windows[active_window].read()
+
 
     layout_restock = [
         [sg.T('RESTOCKING')],
         [sg.B('Close')]
     ]
-    if event
+    if event == sg.WIN_CLOSED:
+        break
+
     return sg.Window('Restock', layout_restock, resizable=True) #, finalize=True)

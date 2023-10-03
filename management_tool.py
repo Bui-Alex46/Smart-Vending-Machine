6feb49d7e30c6380413e58bb7724afa5c4819a7e
Alex-R-Projects <mralexreyes99@gmail.com>
2023-10-03 14:29:59 -0700

    added some code for instructions

---
+++ management_tool.py
@@ -0,0 +1,5 @@
+import smtplib # This will be used to send emails to the restockers
+
+def send_instructions():
+    pass # This function will send out the instructions out to a restocker
+
--- restocker_instructs.py
+++ restocker_instructs.py
@@ -1,10 +1,12 @@
 # This will be the file that takes in the pushed out instructions FROM the management team/tool
 
 # [Here we can place the other files like main, etc]
-
+import send_instructions from management_tool
 
 class Restocker: # In order to get the instructions, the restocker, I think should have some sort of account
     def __init__(self, email ) : # constructor
         self.email = email
+        
+# sending instructions via email
 
 

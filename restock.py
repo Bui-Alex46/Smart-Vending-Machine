ec136e82b47d7f33095f5f90cf15a23c946fe27d
Alex-R-Projects <mralexreyes99@gmail.com>
2023-10-02 17:35:01 -0700

    commented out mistakes

--- restock.py
+++ restock.py
@@ -9,7 +9,7 @@ def create_restock_window():
         [sg.T('RESTOCKING')],
         [sg.B('Close')]
     ]
-    if event == sg.WIN_CLOSED:
-        pass
+    #if event == sg.WIN_CLOSED:
+    #    pass
 
     return sg.Window('Restock', layout_restock, resizable=True) #, finalize=True)

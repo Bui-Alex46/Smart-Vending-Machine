a15feae1698d9e6c3e8b4f28ea0f203b08de89dd
jackthomas00 <40306951+jackthomas00@users.noreply.github.com>
2023-10-31 16:00:39 -0700

    fixed vending machine display

--- pickle_method_vending.py
+++ pickle_method_vending.py
@@ -61,43 +61,42 @@ class ItemSlot:
     def __init__(self, list) -> None:
         self.list = list
 
-def main(): # below are 2 vending machine objects defined with different layout dimensions
-    vending_machine_inventory = pd.read_csv("database\\VendingMachine1.csv")
-    df_itemTable = pd.read_csv("database\\ItemTable.csv", index_col=0, header=0)
+# def main(): # below are 2 vending machine objects defined with different layout dimensions
+#     vending_machine_inventory = pd.read_csv("database\\VendingMachine1.csv")
+#     df_itemTable = pd.read_csv("database\\ItemTable.csv", index_col=0, header=0)
     
-    vending_machine_1_slots = []
-    for items in vending_machine_inventory.values.tolist():
+#     vending_machine_1_slots = []
+#     for items in vending_machine_inventory.values.tolist():
+#         print(items)
+#         item_list = [Item(items[0], df_itemTable.loc[items[1]]["item cost"], df_itemTable.loc[items[1]]["item name"], "11/01/23") for x in range(items[2])]
+#         vending_machine_1_slots.append(ItemSlot(item_list))
+#     # Creating vending machine objects
+#     vending_machine_1 = Vending_Machine(8, 5, "active", 
+#     vending_machine_1_slots, "69")
+#    # vending_machine_2 = Vending_Machine(4, 4, "active", 
+#    # ["A1", "A2", "A3", "A4",
+#    # "B1", "B2", "B3", "B4",
+#    # "C1", "C2", "C3", "C4",
+#    # "D1", "D2", "D3", "D4",], "42")
 
-        item_list = [Item(items[0], df_itemTable.loc[items[1]]["item cost"], df_itemTable.loc[items[1]]["item name"], "11/01/23") for x in range(items[2])]
-        ItemSlot(item_list)
-        print(item_list[0])
-    # Creating vending machine objects
-    vending_machine_1 = Vending_Machine(8, 5, "active", 
-    vending_machine_1_slots, "69")
-   # vending_machine_2 = Vending_Machine(4, 4, "active", 
-   # ["A1", "A2", "A3", "A4",
-   # "B1", "B2", "B3", "B4",
-   # "C1", "C2", "C3", "C4",
-   # "D1", "D2", "D3", "D4",], "42")
+#     # Saving vending machine objects using pickle
+#     with open("vending_machine_1.pkl", "wb") as file: # vending machine #1
+#         pickle.dump(vending_machine_1, file)
+#         file.close()
 
-    # Saving vending machine objects using pickle
-    with open("vending_machine_1.pkl", "wb") as file: # vending machine #1
-        pickle.dump(vending_machine_1, file)
-        file.close()
+#     # with open("vending_machine_2.pkl", "wb") as file: # vending machine #2
+#     #     pickle.dump(vending_machine_2, file)
 
-    # with open("vending_machine_2.pkl", "wb") as file: # vending machine #2
-    #     pickle.dump(vending_machine_2, file)
+#     # Loading vending machine objects from pickle files
+#     with open("vending_machine_1.pkl", "rb") as file:
+#         loaded_vending_machine_1 = pickle.load(file)
+#         print("Loaded Vending Machine 1 layout:", loaded_vending_machine_1.row, "x", loaded_vending_machine_1.column)
+#         print("Loaded Vending Machine 1 ID:", loaded_vending_machine_1.get_vending_machine_id())
 
-    # Loading vending machine objects from pickle files
-    with open("vending_machine_1.pkl", "rb") as file:
-        loaded_vending_machine_1 = pickle.load(file)
-        print("Loaded Vending Machine 1 layout:", loaded_vending_machine_1.row, "x", loaded_vending_machine_1.column)
-        print("Loaded Vending Machine 1 ID:", loaded_vending_machine_1.get_vending_machine_id())
+#     # with open("vending_machine_2.pkl", "rb") as file:
+#     #     loaded_vending_machine_2 = pickle.load(file)
+#     #     print("Loaded Vending Machine 2 layout:", loaded_vending_machine_2.row, "x", loaded_vending_machine_2.column)
+#     #     print("Loaded Vending Machine 2 ID:", loaded_vending_machine_2.get_vending_machine_id())
 
-    # with open("vending_machine_2.pkl", "rb") as file:
-    #     loaded_vending_machine_2 = pickle.load(file)
-    #     print("Loaded Vending Machine 2 layout:", loaded_vending_machine_2.row, "x", loaded_vending_machine_2.column)
-    #     print("Loaded Vending Machine 2 ID:", loaded_vending_machine_2.get_vending_machine_id())
-
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()

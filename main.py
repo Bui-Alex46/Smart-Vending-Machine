432edd0d093d9cd536603437defe0ffff5150f0b
Alex-R-Projects <mralexreyes99@gmail.com>
2023-10-31 14:13:37 -0700

    commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import PySimpleGUI as sg
-
+import pickle
 from vending_machine import run_vending_machine
 from restock import run_restock
 from management import run_management
@@ -34,7 +34,23 @@ vending_machines = [1, 2]
 windows = {
     'Choose Machine' : choose_vending_machine_window(vending_machines)
 }
+# Saving vending machine objects using pickle
+# with open("vending_machine_1.pkl", "wb") as file: # vending machine #1
+#     pickle.dump(vending_machine_1, file)
 
+# with open("vending_machine_2.pkl", "wb") as file: # vending machine #2
+#     pickle.dump(vending_machine_2, file)
+
+# Loading vending machine objects from pickle files
+with open("vending_machine_1.pkl", "rb") as file:
+    loaded_vending_machine_1 = pickle.load(file)
+    print("Loaded Vending Machine 1 layout:", loaded_vending_machine_1.row, "x", loaded_vending_machine_1.column)
+    print("Loaded Vending Machine 1 ID:", loaded_vending_machine_1.get_vending_machine_id())
+
+# with open("vending_machine_2.pkl", "rb") as file:
+#     loaded_vending_machine_2 = pickle.load(file)
+#     print("Loaded Vending Machine 2 layout:", loaded_vending_machine_2.row, "x", loaded_vending_machine_2.column)
+#     print("Loaded Vending Machine 2 ID:", loaded_vending_machine_2.get_vending_machine_id())
 def main():
     active_window = 'Choose Machine'
     
--- pickle_method_vending.py
+++ pickle_method_vending.py
@@ -62,18 +62,18 @@ class ItemSlot:
         self.list = list
 
 def main(): # below are 2 vending machine objects defined with different layout dimensions
-    # vending_machine_inventory = pd.read_csv("database\\VendingMachine1.csv")
-    # df_itemTable = pd.read_csv("database\\ItemTable.csv", index_col=0, header=0)
+    vending_machine_inventory = pd.read_csv("database\\VendingMachine1.csv")
+    df_itemTable = pd.read_csv("database\\ItemTable.csv", index_col=0, header=0)
     
-    # vending_machine_1_slots = []
-    # for items in vending_machine_inventory.values.tolist():
+    vending_machine_1_slots = []
+    for items in vending_machine_inventory.values.tolist():
 
-    #     item_list = [Item(items[0], df_itemTable.loc[items[1]]["item cost"], df_itemTable.loc[items[1]]["item name"], "11/01/23") for x in range(items[2])]
-    #     ItemSlot(item_list)
-    #     print(item_list[0])
-    # # Creating vending machine objects
-    # vending_machine_1 = Vending_Machine(8, 5, "active", 
-    # vending_machine_1_slots, "69")
+        item_list = [Item(items[0], df_itemTable.loc[items[1]]["item cost"], df_itemTable.loc[items[1]]["item name"], "11/01/23") for x in range(items[2])]
+        ItemSlot(item_list)
+        print(item_list[0])
+    # Creating vending machine objects
+    vending_machine_1 = Vending_Machine(8, 5, "active", 
+    vending_machine_1_slots, "69")
    # vending_machine_2 = Vending_Machine(4, 4, "active", 
    # ["A1", "A2", "A3", "A4",
    # "B1", "B2", "B3", "B4",
@@ -81,8 +81,9 @@ def main(): # below are 2 vending machine objects defined with different layout 
    # "D1", "D2", "D3", "D4",], "42")
 
     # Saving vending machine objects using pickle
-    # with open("vending_machine_1.pkl", "wb") as file: # vending machine #1
-    #     pickle.dump(vending_machine_1, file)
+    with open("vending_machine_1.pkl", "wb") as file: # vending machine #1
+        pickle.dump(vending_machine_1, file)
+        file.close()
 
     # with open("vending_machine_2.pkl", "wb") as file: # vending machine #2
     #     pickle.dump(vending_machine_2, file)
